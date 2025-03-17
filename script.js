07319fa54fc5ff24f25e8b47139c92b427bf2c28
Victor Cruz <victorcb3380@gmail.com>
2025-03-17 15:44:47 +0000

    script1.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ async function sendMessage() {
 
   try {
     const response = await fetch(
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyCNWNXE9505VaRKBb4zkw9UHaJ0zNSxiCE',
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyA2sOpZnLQr94Vg5JYrr5xVjYiFGTPjlcQ',
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
